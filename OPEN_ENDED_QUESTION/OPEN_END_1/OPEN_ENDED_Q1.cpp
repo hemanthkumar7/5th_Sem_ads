6122d6ca22b4a7d3440e2d379f6017719899bb5d
hemanthkumar7 <53857478+hemanthkumar7@users.noreply.github.com>
2020-12-30 12:23:19 +0530

    Add files via upload

---
+++ OPEN_ENDED_QUESTION/OPEN_END_1/OPEN_ENDED_Q1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+void findElements(int arr[], int n) 
+{ 
+    for (int i = 0; i < n; i++) 
+    { 
+        int count = 0; 
+        for (int j = 0; j < n; j++) 
+            if (arr[j] > arr[i]) 
+                count++; 
+  
+        if (count >= 2) 
+            cout << arr[i] << " "; 
+    } 
+} 
+  
+int main() 
+{ 
+    int n;
+    int arr[n];
+    
+    cout << "Size of array:"<<endl;
+    cin >> n;;
+    
+    cout << "Enter the elements:"<<endl;
+    for (int i=0;i<n;i++)
+       cin >> arr[i];
+    
+    cout << "Array after excluding 2 greatest elements:"<<endl;   
+    findElements(arr, n); 
+    return 0; 
+}
